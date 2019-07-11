0c6419867768b8d09a8f7f9928188539d6bf76ae
ethan-humphries <ethan.humphries1993@hotmail.com>
2019-07-11 20:50:52 +1000

    add mock booking data add data table and bind data add sorting to data table

--- src/app/components/view-bookings/view-bookings.module.ts
+++ src/app/components/view-bookings/view-bookings.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViewBookingsComponent } from './view-bookings.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     ViewBookingsComponent
   ],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule
   ],
   exports: [
     ViewBookingsComponent
