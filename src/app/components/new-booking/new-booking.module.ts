e7f5705a7d5d630408813e8480afeaa1d3adf713
ssjwah <52654765+ssjwah@users.noreply.github.com>
2019-07-13 22:26:41 +1000

    new booking component
    Change to new booking component

--- src/app/components/new-booking/new-booking.module.ts
+++ src/app/components/new-booking/new-booking.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { NewBookingComponent } from './new-booking.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
     NewBookingComponent
   ],
   imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatDatepickerModule
   ],
   exports: [
     NewBookingComponent
