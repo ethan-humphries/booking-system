e09cfa79c3825ce1731a1af257544127b652f36a
ethan-humphries <ethan.humphries1993@hotmail.com>
2019-07-15 16:47:44 +1000

    decouple tiles from parent component

--- src/app/components/dashboard-overview/dashboard-overview.component.ts
+++ src/app/components/dashboard-overview/dashboard-overview.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-overview.component.scss']
 })
 export class DashboardOverviewComponent implements OnInit {
-
+  title =  "Summary"
   constructor() { }
 
   ngOnInit() {
--- src/app/components/overview-tile/overview-tile.component.ts
+++ src/app/components/overview-tile/overview-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-overview-tile',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview-tile.component.scss']
 })
 export class OverviewTileComponent implements OnInit {
-
+  @Input() title: string;
+  
   constructor() { }
 
   ngOnInit() {
