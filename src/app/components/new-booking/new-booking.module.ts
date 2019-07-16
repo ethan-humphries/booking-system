379480beab669c552c348a8321cdd98c3c574216
ssjwah <52654765+ssjwah@users.noreply.github.com>
2019-07-16 21:20:04 +1000

    updated .ts file

--- src/app/components/new-booking/new-booking.module.ts
+++ src/app/components/new-booking/new-booking.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NewBookingComponent } from './new-booking.component';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { BookingService } from 'src/app/services/bookings/booking-service.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
